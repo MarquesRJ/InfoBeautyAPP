d26e5b11ffa7c315d5cd9833ddc3f29d5764d4eb
InfoBeaty <viniciuspinheiro457@gmail.com>
2021-03-27 20:23:26 -0300

    app InfoBeauty

--- app/src/main/java/com/example/infobeauty/MainActivity.java
+++ app/src/main/java/com/example/infobeauty/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,13 +16,13 @@ public class MainActivity extends AppCompatActivity {
 
     public void telaCadastro(View view){
 
-        Intent intent = new Intent(this, Cadastro.class);
+        Intent intent = new Intent(this, CadastroUsuario.class);
         startActivity(intent);
     }
 
     public void telaLogin(View view){
 
-        Intent intent = new Intent(this, Login.class);
+        Intent intent = new Intent(this, LoginUsuario.class);
         startActivity(intent);
     }
 }
